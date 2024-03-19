052efa2d97f431ee5e4ecc1accf89ee16967ed99
FedoseevDS <dmsr.fedoseev@gmail.com>
2024-03-19 15:58:13 +0300

    удалил папку dist

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,10 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   base: '/wheel/',
   plugins: [react()],
   server: {
     port: 5000,
   },
-  ...(mode === 'production'
-    ? {
-        build: {
-          outDir: 'dist',
-        },
-      }
-    : {}),
-}));
+});
