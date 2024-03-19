2122b9f38470209953285e9d4b06c75206886ddb
FedoseevDS <dmsr.fedoseev@gmail.com>
2024-03-19 15:52:43 +0300

    поправил конфигурацию vita

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,17 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   base: '/wheel/',
   plugins: [react()],
   server: {
     port: 5000,
   },
-});
+  ...(mode === 'production'
+    ? {
+        build: {
+          outDir: 'dist',
+        },
+      }
+    : {}),
+}));
